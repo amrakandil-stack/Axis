5089b51fd91a1a4f86a9100fde010bfa9a5cb37d
amrakandil-stack <amr.a.kandil@gmail.com>
2026-01-18 18:48:33 +0400

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
