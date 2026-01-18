2df5faffce2e7c84bf713e53d53370489cf968ce
amrakandil-stack <amr.a.kandil@gmail.com>
2026-01-18 16:54:43 +0400

    Show navbar on all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -7,6 +9,8 @@ import Contact from "./pages/Contact";
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
